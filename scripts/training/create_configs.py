3c068d3979d4848ccd20b267079dec06bd343d91
Sarah Schuhegger <s.schuhegger@dkfz-heidelberg.de>
2021-05-19 17:18:20 +0200

    update loh-experiment

--- scripts/training/create_configs.py
+++ scripts/training/create_configs.py
@@ -17,16 +17,17 @@ from scripts.dataset.custom_transformations import *
 gaussian_noise = GaussNoise(std_min=0, std_max=0.04, min_value=-1, max_value=1, p=0.5) # equivalent to max gaussian std noise of 50 HU 
 shift_hu = ShiftHU(shift_limit=0.08, min_value=-1, max_value=1, p=0.5) # equivalent to max shift of 100 HU 
 scale_hu = ScaleHU(scale_delta=0.2, min_value=-1, max_value=1, p=0.5)
-add_frame = Ad, p=0.dFrame(p=0.25)
+add_frame = AddFrame(p=0.25, r_circle=0.75, fill_value=-1)
 flip = A.Flip(p=0.5) # Flip the input either horizontally, vertically or both
 transpose = A.Transpose(p=0.5) 
-shift_scale_rotate = A.ShiftScaleRotate(shift_limit=0, scale_limit=0.2, rotate_limit=10, p=0.5, border_mode=cv2.BORDER_CONSTANT)
+shift_scale_rotate = A.ShiftScaleRotate(shift_limit=0, scale_limit=0.2, rotate_limit=10, p=0.5, 
+                                        border_mode=cv2.BORDER_REFLECT_101)
 gaussian_blure = A.GaussianBlur(blur_limit=(3, 7), sigma_limit=0.5, always_apply=False, p=0.5)
 
 custom_transform = transforms.Compose([gaussian_noise, shift_hu, scale_hu, add_frame])
 albumentation_transform =  A.Compose([flip, transpose, shift_scale_rotate, gaussian_blure])
 
-    
+
 custom_transform_params = {}
 for i in range(len(custom_transform.__dict__["transforms"])): 
         custom_transform_params[custom_transform.__dict__["transforms"][i].__class__] =  custom_transform.__dict__["transforms"][i].__dict__
@@ -62,8 +63,8 @@ config = {
     "base_model": "vgg", 
     "   ": "\n*******************************************************", 
 
-    "batch_size": 21,
-    "effective_batch_size": 21, 
+    "batch_size": 20, # TODO
+    "effective_batch_size": 20, # TODO 
     "equidistance_range": [5, 100], # in mmm 
     "num_slices": 12, 
 
@@ -99,6 +100,22 @@ if __name__ == "__main__":
         2: {"alpha_h": 0.6, "beta_h": 0.03, "name": "loh-experiment-0.03b-0.6a.p"},
         3: {"alpha_h": 0.8, "beta_h": 0.04, "name": "loh-experiment-0.4b-0.8a.p"},
     }
+    
+    
+    experiments = {0: {'alpha': 0.001, 'beta': 0.001, 'name': 'loh-0.001a-0.001b.p'},
+ 1: {'alpha': 0.001, 'beta': 0.01, 'name': 'loh-0.001a-0.01b.p'},
+ 2: {'alpha': 0.001, 'beta': 0.1, 'name': 'loh-0.001a-0.1b.p'},
+ 3: {'alpha': 0.01, 'beta': 0.001, 'name': 'loh-0.01a-0.001b.p'},
+ 4: {'alpha': 0.01, 'beta': 0.01, 'name': 'loh-0.01a-0.01b.p'},
+ 5: {'alpha': 0.01, 'beta': 0.1, 'name': 'loh-0.01a-0.1b.p'},
+ 6: {'alpha': 0.1, 'beta': 0.001, 'name': 'loh-0.1a-0.001b.p'},
+ 7: {'alpha': 0.1, 'beta': 0.01, 'name': 'loh-0.1a-0.01b.p'},
+ 8: {'alpha': 0.1, 'beta': 0.1, 'name': 'loh-0.1a-0.1b.p'},
+ 9: {'alpha': 1, 'beta': 0.001, 'name': 'loh-1a-0.001b.p'},
+ 10: {'alpha': 1, 'beta': 0.01, 'name': 'loh-1a-0.01b.p'},
+ 11: {'alpha': 1, 'beta': 0.1, 'name': 'loh-1a-0.1b.p'},
+ 12: {'alpha': 0.1, 'beta': 0.0001, 'name': 'loh-0.1a-0.0001b.p'},
+ 13: {'alpha': 0.1, 'beta': 1, 'name': 'loh-0.1a-1b.p'}}
 
     save_path_folder = "../../src/configs/" + mode + "/" +  config["model_name"] + "/" 
     if not os.path.exists(save_path_folder): os.mkdir(save_path_folder)
