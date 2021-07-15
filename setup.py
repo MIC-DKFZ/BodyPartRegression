490c1680a9f7165e083de77a145f0dd6630ee4c2
Sarah Schuhegger <s.schuhegger@dkfz-heidelberg.de>
2021-07-15 10:53:33 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+
+def resolve_requirements(file):
+    requirements = []
+    with open(file) as f:
+        req = f.read().splitlines()
+        for r in req:
+            if r.startswith("-r"):
+                requirements += resolve_requirements(
+                    os.path.join(os.path.dirname(file), r.split(" ")[1]))
+            else:
+                requirements.append(r)
+    return requirements
+
+# load requirements 
+requirements = resolve_requirements(os.path.join(os.path.dirname(__file__), 'requirements.txt'))
+
+# read readme 
+with open("README.md", "r") as f: 
+    readme = f.read() 
+
+setup(
+    name = "bpreg",
+    version = "0.0.1",
+    packages=find_packages(), 
+    include_package_data=True, 
+    test_suite="unittest", 
+    install_requires=requirements, 
+    long_description=readme, 
+    author = "Division of Medical Image Computing, German Cancer Research Center",
+    maintainer_email = "",
+    entry_points={'bpreg_predict = scripts.bpreg_inference'})
