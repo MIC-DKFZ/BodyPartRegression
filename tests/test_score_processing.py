d3ce1fb2ee1e635a72cfb8433b7a4393a9c5bb0e
Sarah Schuhegger <s.schuhegger@dkfz-heidelberg.de>
2021-07-22 17:58:40 +0200

    add tests

--- tests/test_score_processing.py
+++ tests/test_score_processing.py
@@ -1,45 +1,50 @@
 import numpy as np
-import sys, os
-
-sys.path.append("../")
 from bpreg.score_processing import Scores
 
-scores1 = np.array([2, -2, -1, 0, 2, 4, 7, 10, 13, 16, 20], dtype=float)
-scores2 = np.array([50, 61, 72, 83, 90, 105, 95, 90, 85], dtype=float)
-scores3 = np.array(
-    [5, 2, -10, 0, 10, 20, 30, 40, 50, 60, 80, 90, 100, 110, -10, -10], dtype=float
-)
-scores4 = np.array(
-    [110, 100, -10, 0, 10, 20, 30, 40, 50, 60, 80, 90, 100, 110, -10, -10], dtype=float
-)
+def create_scores(slope, error, n, zspacing, b=0): 
+    epsilon = np.random.normal(0, error, n) 
+    x = np.arange(0, n, 1) 
+    y = x*zspacing*slope + epsilon + b
+    
+    return y 
 
+def test_basic_score_processing(): 
+    slope=0.11
+    zspacing=1
+    y = create_scores(slope=slope, error=0.2, n=100, zspacing=zspacing)
+    
+    scores = Scores(y, zspacing=zspacing, transform_min=0, transform_max=100, slope_mean=slope)
 
-# TODO - fix tests!!!
+    # Check if no missing values are in cleaned slice scores 
+    assert np.sum(np.isnan(scores.values)*1) == 0
 
-"""
-def test_valid_range(): 
-    scores = Scores(scores1, zspacing=20, smoothing_sigma=1)
-    np.testing.assert_equal(np.where(np.isnan(scores.values))[0], np.array([0]))
+    # check if zspacing is valid 
+    assert scores.valid_zspacing == 1
 
-    scores = Scores(scores2, zspacing=40, smoothing_sigma=1)
-    np.testing.assert_equal(np.where(np.isnan(scores.values))[0], np.array([6, 7, 8]))
+def test_tail_missing_values(): 
+    zspacing=4
+    slope=0.11
+    y = create_scores(slope=0.11, error=0.2, n=400, zspacing=zspacing, b=-20)
+    y = [20, 20, 20] + list(y) + [20, 20, 20] 
+    
+    scores = Scores(y, zspacing=zspacing, transform_min=0, transform_max=100, slope_mean=slope)
+    assert np.sum(np.isnan(scores.values[0:3])) == 3
+    assert np.sum(np.isnan(scores.values[-3:])) == 3
 
-    scores = Scores(scores3, zspacing=80, smoothing_sigma=0)
-    np.testing.assert_equal(np.where(np.isnan(scores.values))[0], np.array([0, 1, 14, 15]))
+def test_transform(): 
+    y = np.arange(0, 10)
+    scores = Scores(y, zspacing=1, transform_min=1, transform_max=8, slope_mean=1)
+    assert scores.original_transformed_values[1] == 0
+    assert scores.original_transformed_values[-2] == 100
 
-    scores = Scores(scores4, zspacing=80, smoothing_sigma=0)
-    np.testing.assert_equal(np.where(np.isnan(scores.values))[0], np.array([0, 1, 14, 15]))
-"""
-
-
-def test_slope():
-    pass
-
-
-def test_transform():
-    pass
-
-
-def test_cut_extreme_diffs():
-    pass
+def test_valid_zspacing(): 
+    slope=0.11
+    zspacing=1
+    y = create_scores(slope=slope, error=0.2, n=100, zspacing=zspacing)
+    
+    scores = Scores(y, zspacing=zspacing, transform_min=0, transform_max=100, slope_mean=2*slope)
+    assert scores.valid_zspacing==0
+    
+    scores = Scores(y, zspacing=zspacing, transform_min=0, transform_max=100, slope_mean=0.5*slope)
+    assert scores.valid_zspacing==0
 
